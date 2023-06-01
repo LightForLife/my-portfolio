859182835731958cb1deb55438163954c29c37d3
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2023-06-01 21:03:32 +0300

    fix fover hero buttons

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -29,7 +29,7 @@ export default function Hero({}: Props) {
         height={600}
         alt="Foto of the author"
       />
-      <div>
+      <div className="z-20">
         <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
           Frontend Developer
         </h2>
