346f07739a1eea1d04163252c52a69cf258428ec
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2023-06-01 19:49:34 +0300

    add foto andcircles for hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -2,6 +2,8 @@
 import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
+import Image from "next/image";
+import foto4 from "../../../public/foto4.png";
 
 type Props = {};
 
@@ -17,8 +19,15 @@ export default function Hero({}: Props) {
   });
 
   return (
-    <div>
+    <div className="h-screen flex flex-col space-y-8 items-center justify-center text-center overflow-hidden">
       <BackgroundCircles />
+      <Image
+        className="relative rounded-full h-32 w-32 mx-auto object-cover"
+        src={foto4}
+        width={600}
+        height={600}
+        alt="Picture of the author"
+      />
       <h1>
         <span>{text}</span>
         <Cursor cursorColor="#F7AB0A" />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,11 @@ import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] text-white h-screen">
+    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
       {/* Header */}
       <Header />
-
       {/* Hero */}
-      <section id="hero">
+      <section id="hero" className="snap-center">
         <Hero />
       </section>
       {/* About */}
@@ -20,6 +19,6 @@ export default function Home() {
       {/* Projects */}
 
       {/* Contact me */}
-    </div>
+    </main>
   );
 }
