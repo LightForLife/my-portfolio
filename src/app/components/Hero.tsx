2065c32b9cabc908e7e1668e978dcbeea9edaea8
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2023-06-01 20:16:47 +0300

    add buttons for hero

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,7 +3,8 @@ import React from "react";
 import { Cursor, useTypewriter } from "react-simple-typewriter";
 import BackgroundCircles from "./BackgroundCircles";
 import Image from "next/image";
-import foto4 from "../../../public/foto4.png";
+import foto from "../../../public/my-foto.png";
+import Link from "next/link";
 
 type Props = {};
 
@@ -23,15 +24,34 @@ export default function Hero({}: Props) {
       <BackgroundCircles />
       <Image
         className="relative rounded-full h-32 w-32 mx-auto object-cover"
-        src={foto4}
+        src={foto}
         width={600}
         height={600}
-        alt="Picture of the author"
+        alt="Foto of the author"
       />
-      <h1>
-        <span>{text}</span>
-        <Cursor cursorColor="#F7AB0A" />
-      </h1>
+      <div>
+        <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
+          Frontend Developer
+        </h2>
+        <h1 className="text-5xl lg:text-6xl font-semibold px-10">
+          <span className="mr-3">{text}</span>
+          <Cursor cursorColor="#F7AB0A" />
+        </h1>
+        <div className="pt-5">
+          <Link href="#about">
+            <button className="heroButton">About</button>
+          </Link>
+          <Link href="#experience">
+            <button className="heroButton">Experience</button>
+          </Link>
+          <Link href="#skills">
+            <button className="heroButton">Skills</button>
+          </Link>
+          <Link href="#projects">
+            <button className="heroButton">Projects</button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
