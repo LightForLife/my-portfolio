c5a6d7567f4e8d3e6312b1b28fcf883593bc3b92
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2023-06-02 18:08:36 +0300

    add experience section

--- src/app/components/ExperienceCard.tsx
+++ src/app/components/ExperienceCard.tsx
@@ -6,12 +6,47 @@ type Props = {};
 
 export default function ExperienceCard({}: Props) {
   return (
-    <article>
+    <article className="flex flex-col rounded-lg items-center space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] p-10 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 overflow-hidden">
       <motion.img
+        initial={{ y: -100, opacity: 0 }}
+        transition={{ duration: 1.2 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: true }}
         className="h-32 w-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center"
         src="https://34travel.me/media/upload/images/2020/MAY/arch2020/1.jpg"
         alt=""
       />
+      <div className="px-0 md:px-10">
+        <h4 className="text-4xl font-light">CEO of PAPAFAM</h4>
+        <p className="font-bold text-2xl mt-1">PAPAFAM</p>
+        <div className="flex space-x-2 my-2">
+          <img
+            className="h-10 w-10 rounded-full"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjtiNdPR3dcbHaaMm0Jd1lK8YFispcnVIACKhD8eZbu4I4xnzQaKeILCI4d6Io46ceAIw&usqp=CAU"
+            alt=""
+          />
+          <img
+            className="h-10 w-10 rounded-full"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjtiNdPR3dcbHaaMm0Jd1lK8YFispcnVIACKhD8eZbu4I4xnzQaKeILCI4d6Io46ceAIw&usqp=CAU"
+            alt=""
+          />
+          <img
+            className="h-10 w-10 rounded-full"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRjtiNdPR3dcbHaaMm0Jd1lK8YFispcnVIACKhD8eZbu4I4xnzQaKeILCI4d6Io46ceAIw&usqp=CAU"
+            alt=""
+          />
+        </div>
+        <p className="uppercase py-5 space-y-4 ml-5 text-lg">
+          Started work... - Ended...
+        </p>
+      </div>
+      <ul className="list-disc space-y-4 msl-5 text-lg">
+        <li>Summary pointsSummary pointsSummary points</li>
+        <li>Summary pointsSummary pointsSummary points</li>
+        <li>Summary pointsSummary pointsSummary points</li>
+        <li>Summary pointsSummary pointsSummary points</li>
+        <li>Summary pointsSummary pointsSummary pointss</li>
+      </ul>
     </article>
   );
 }
