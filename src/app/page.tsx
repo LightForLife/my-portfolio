8dd296ce0b561ae1c39baf917af1750f876d6fd4
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2023-06-03 19:07:31 +0300

    add section projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import Experience from "./components/Experience";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 export default function Home() {
   return (
@@ -27,7 +28,9 @@ export default function Home() {
         <Skills />
       </section>
       {/* Projects */}
-
+      <section id="projects" className="snap-start">
+        <Projects />
+      </section>
       {/* Contact me */}
     </main>
   );
