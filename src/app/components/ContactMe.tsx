fc0daa4220306f9d161c4ef2475437f0420645dd
Artur Levchenko <99216307+LightForLife@users.noreply.github.com>
2023-06-11 17:48:44 +0300

    add submit form and buttom to top

--- src/app/components/ContactMe.tsx
+++ src/app/components/ContactMe.tsx
@@ -1,9 +1,23 @@
+"use client";
 import React from "react";
 import { PhoneIcon, MapPinIcon, EnvelopeIcon } from "@heroicons/react/24/solid";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+};
 
 type Props = {};
 
 function ContactMe({}: Props) {
+  const { register, handleSubmit } = useForm<Inputs>();
+  const onSubmit: SubmitHandler<Inputs> = (formData) => {
+    window.location.href = `mailto:arturlvchnk@gmail.com?subject=${formData.subject}&body=Hi, my name is ${formData.name}. ${formData.message} (${formData.email})`;
+  };
+
   return (
     <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
       <h3 className="absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
@@ -28,13 +42,41 @@ function ContactMe({}: Props) {
             <p className="text-2xl">123 Developer Lane</p>
           </div>
         </div>
-        <form className="flex flex-col space-y-2 w-fit mx-auto">
-          <div>
-            <input type="text" /> <input type="text" />
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-col space-y-2 w-fit mx-auto"
+        >
+          <div className="flex space-x-2">
+            <input
+              {...register("name")}
+              placeholder="Name"
+              className="contactInput"
+              type="text"
+            />
+            <input
+              {...register("email")}
+              placeholder="Email"
+              className="contactInput"
+              type="text"
+            />
           </div>
-          <input type="text" />
-          <textarea></textarea>
-          <button>Submit</button>
+          <input
+            {...register("subject")}
+            placeholder="Subject"
+            className="contactInput"
+            type="text"
+          />
+          <textarea
+            {...register("message")}
+            placeholder="Message"
+            className="contactInput"
+          ></textarea>
+          <button
+            type="submit"
+            className="bg-[#F7AB0A] py-5 px-10 rounded-md text-black font-bold text-lg"
+          >
+            Submit
+          </button>
         </form>
       </div>
     </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,10 +5,16 @@ import Experience from "./components/Experience";
 import Skills from "./components/Skills";
 import Projects from "./components/Projects";
 import ContactMe from "./components/ContactMe";
+import Link from "next/link";
+import Image from "next/image";
+import foto from "../../public/good.jpg";
 
 export default function Home() {
   return (
-    <main className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-scroll z-0">
+    <main
+      className="bg-[rgb(36,36,36)] text-white h-screen snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 
+    scrollbar-thumb-[#F7AB0A]/80"
+    >
       {/* Header */}
       <Header />
       {/* Hero */}
@@ -36,6 +42,20 @@ export default function Home() {
       <section id="contact" className="snap-start">
         <ContactMe />
       </section>
+
+      <Link href="#hero">
+        <footer className="sticky bottom-5 w-full cursor-pointer">
+          <div className="flex items-center justify-center">
+            <Image
+              src={foto}
+              width={600}
+              height={600}
+              alt="Buttom to top"
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+            ></Image>
+          </div>
+        </footer>
+      </Link>
     </main>
   );
 }
